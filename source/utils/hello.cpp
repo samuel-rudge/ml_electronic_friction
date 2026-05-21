bfdeace825da3281923164af8b5f05a489a8a790
samuel-rudge <samuel.rudge@physik.uni-freiburg.de>
2026-05-21 16:23:18 +0200

    first files

---
+++ source/utils/hello.cpp
@@ -0,0 +1,7 @@
+// hello.cpp
+#include <iostream> // include standard input/output library
+
+int main() {        // main function: entry point of any C++ program
+    std::cout << "Hello, HEOM in C++!" << std::endl; // print to console
+    return 0;       // exit code 0 means successsc
+}
