5bb6b20af5ed5d4eb852df88d969e9c828b711ca
samuel-rudge <samuel.rudge@physik.uni-freiburg.de>
2026-05-26 10:15:21 +0200

    worked on json stuff

--- source/main_harmonic.cpp
+++ source/main_harmonic.cpp
@@ -1,21 +1,7 @@
 #include "utils/math_utils.h"
+#include "utils/readwrite_traj.h"
 #include <iostream> // for std::cout
 #include <vector>
-#include <fstream>
-
-void write_data(
-    const std::vector<double>& time_vec,
-    const std::vector<double>& x,
-    const std::vector<double>& p
-)
-{
-    std::ofstream file("traj.dat");
-
-    for (std::size_t i{ 0 }; i < time_vec.size(); ++i)
-    {
-        file << time_vec[i] << " " << x[i] << " " << p[i] << "\n"; 
-    }
-}
 
 void one_timestep(
     double& x,
